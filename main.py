70563ba8ff8f8a6a2240b80eda168a2c93bc6b9a
chip-aga314 <34406764+chip-aga314@users.noreply.github.com>
2023-02-15 23:31:31 +0000

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello():
     """Return a friendly HTTP greeting."""
     print("I am inside hello world")
-    return 'Hello World! CipAga'
+    return 'Hello World! Hooorah'
 
 @app.route('/echo/<name>')
 def echo(name):
